c2ecf724b316be1e489627df82c07e16bfcf148e
harshsahu383 <harshsahu1917@gmail.com>
2025-05-25 21:24:25 +0530

    Q2.js

---
+++ JavaScript/Practice/Q2.js
@@ -0,0 +1,37 @@
+// **** DO NOT CHANGE the values *****
+const a = 18;
+const b = 24;
+
+// Addition of two values
+function add(a,b) {
+  return a + b ;
+}
+
+// Subtract small value from larger one
+function subtract(a,b) {
+  return a - b;
+}
+
+function multiply(a,b) {
+  return a * b;
+}
+
+// Divide larger value by small
+function divide(a , b) {
+  return b / a;
+}
+
+// Increase value of a by 1
+function increment(a,b) {
+  return a++
+}
+
+// Decrease value of b by 1
+function decrement(a,b) {
+  return b--
+}
+
+// Divide larger value by small to find the reminder
+function reminder(a,b) {
+  return b % a;
+}
