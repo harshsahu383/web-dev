9ffdffb4be76d418b1b427767ce886a05d404a53
harshsahu383 <harshsahu1917@gmail.com>
2025-07-31 23:09:05 +0530

    empty

--- JavaScript/Q25.js
+++ JavaScript/Q25.js
@@ -1,1 +1,12 @@
-let a = 10;
+// // Practice Question
+// Write a function to find minimium in array
+let arr = [5,2,5,4,8,4];
+let res = arr.reduce((res,el) => {
+    if(res < el){
+        return res;
+    }
+    else{
+        return el;
+    }
+})
+console.log(res);
